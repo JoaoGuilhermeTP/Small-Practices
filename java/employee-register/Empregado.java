5a71cd2f37cac568f90ca8a511fe3cfbf9f609a8
JoaoGuilhermeTP <adm.joaoguilherme@gmail.com>
2024-04-08 00:21:21 -0300

    Adicionados comentários ao código

--- java/employee-register/Empregado.java
+++ java/employee-register/Empregado.java
@@ -1,29 +1,40 @@
 public class Empregado {
-  
+
+    // Declarar atributos
     private String nome;
     private double salario;
     private Empresa empresa;
-  
+
+    // SETTERS
+
+    // Método para atribuir nome do empregado
     public void setNome(String nome) {
       this.nome = nome;
     }
-  
+    
+    // Método para atribuir salário do empregado
     public void setSalario(double salario) {
       this.salario = salario;
     }
-  
+    
+    // Método para atribuir empresa do empregado
     public void setEmpresa(Empresa empresa) {
       this.empresa = empresa;
     }
-  
+    
+    // GETTERS
+
+    // Método para obter nome do empregado
     public String getNome() {
       return this.nome;
     }
-  
+    
+    // Método para obter salário do empregado
     public double getSalario() {
       return this.salario;
     }
   
+    // Método para obter empresa do empregado
     public Empresa getEmpresa() {
       return this.empresa;
     }
--- java/employee-register/Empresa.java
+++ java/employee-register/Empresa.java
@@ -1,29 +1,40 @@
 public class Empresa {
-  
+
+  // Declarar atributos
     private String razaoSocial;
     private String cnpj;
     private String fone;
-  
+
+    //SETTERS
+
+    // Método para atribuir razão social da empresa
     public void setRazaoSocial(String razaoSocial) {
       this.razaoSocial = razaoSocial;
     }
-  
+    
+    // Método para atribuir telefone da empresa
     public void setFone(String fone) {
       this.fone = fone;
     }
-  
+    
+    // Método para atribuir CNPJ da empresa
     public void setCnpj(String cnpj) {
       this.cnpj = cnpj;
     }
+
+    // GETTERS
   
+    // Método para obter razão social da empresa
     public String getRazaoSocial() {
       return this.razaoSocial;
     }
-  
+    
+    // Método para obter CNPJ da empresa
     public String getCnpj() {
       return this.cnpj;
     }
-  
+    
+    // Método para obter telefone da empresa
     public String getFone() {
       return this.fone;
     }
